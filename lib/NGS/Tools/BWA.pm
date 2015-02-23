cb178a98b70e4a2b70bc9d4f36fb48864fdc5799
Richard de Borja <richard.deborja@gmail.com>
2015-02-23 10:37:44 -0500

    added new version to VERSION tag

--- lib/NGS/Tools/BWA.pm
+++ lib/NGS/Tools/BWA.pm
@@ -18,11 +18,11 @@ NGS::Tools::BWA - Perl wrapper for the BWA alignment tool.
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 =head1 SYNOPSIS
 
